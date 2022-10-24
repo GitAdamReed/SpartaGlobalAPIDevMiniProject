c42b39d243863da0865127c4787bd30dbbfaeb63
Jai Kothari <jai25@live.co.uk>
2022-10-24 11:12:12 +0100

    Initial Commit.

---
+++ APIMiniProject/APIMiniProject/Models/NorthwindContext.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIMiniProject.Models;
+
+public partial class NorthwindContext : DbContext
+{
+    public NorthwindContext()
+    { }
+
+    public NorthwindContext(DbContextOptions<NorthwindContext> options)
+        : base(options)
+    { }
+
+    public virtual DbSet<Employee> Employees { get; set; } = null!;
+    public virtual DbSet<Region> Regions { get; set; } = null!;
+    public virtual DbSet<Territory> Territories { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasIndex(e => e.LastName, "LastName");
+
+            entity.HasIndex(e => e.PostalCode, "PostalCode");
+
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+
+            entity.Property(e => e.Address).HasMaxLength(60);
+
+            entity.Property(e => e.BirthDate).HasColumnType("datetime");
+
+            entity.Property(e => e.City).HasMaxLength(15);
+
+            entity.Property(e => e.Country).HasMaxLength(15);
+
+            entity.Property(e => e.Extension).HasMaxLength(4);
+
+            entity.Property(e => e.FirstName).HasMaxLength(10);
+
+            entity.Property(e => e.HireDate).HasColumnType("datetime");
+
+            entity.Property(e => e.HomePhone).HasMaxLength(24);
+
+            entity.Property(e => e.LastName).HasMaxLength(20);
+
+            entity.Property(e => e.Notes).HasColumnType("ntext");
+
+            entity.Property(e => e.Photo).HasColumnType("image");
+
+            entity.Property(e => e.PhotoPath).HasMaxLength(255);
+
+            entity.Property(e => e.PostalCode).HasMaxLength(10);
+
+            entity.Property(e => e.Region).HasMaxLength(15);
+
+            entity.Property(e => e.Title).HasMaxLength(30);
+
+            entity.Property(e => e.TitleOfCourtesy).HasMaxLength(25);
+
+            entity.HasOne(d => d.ReportsToNavigation)
+                .WithMany(p => p.InverseReportsToNavigation)
+                .HasForeignKey(d => d.ReportsTo)
+                .HasConstraintName("FK_Employees_Employees");
+
+            entity.HasMany(d => d.Territories)
+                .WithMany(p => p.Employees)
+                .UsingEntity<Dictionary<string, object>>(
+                    "EmployeeTerritory",
+                    l => l.HasOne<Territory>().WithMany().HasForeignKey("TerritoryId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_EmployeeTerritories_Territories"),
+                    r => r.HasOne<Employee>().WithMany().HasForeignKey("EmployeeId").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_EmployeeTerritories_Employees"),
+                    j =>
+                    {
+                        j.HasKey("EmployeeId", "TerritoryId").IsClustered(false);
+
+                        j.ToTable("EmployeeTerritories");
+
+                        j.IndexerProperty<int>("EmployeeId").HasColumnName("EmployeeID");
+
+                        j.IndexerProperty<string>("TerritoryId").HasMaxLength(20).HasColumnName("TerritoryID");
+                    });
+        });
+
+        modelBuilder.Entity<Region>(entity =>
+        {
+            entity.HasKey(e => e.RegionId)
+                .IsClustered(false);
+
+            entity.ToTable("Region");
+
+            entity.Property(e => e.RegionId)
+                .ValueGeneratedNever()
+                .HasColumnName("RegionID");
+
+            entity.Property(e => e.RegionDescription)
+                .HasMaxLength(50)
+                .IsFixedLength();
+        });
+
+        modelBuilder.Entity<Territory>(entity =>
+        {
+            entity.HasKey(e => e.TerritoryId)
+                .IsClustered(false);
+
+            entity.Property(e => e.TerritoryId)
+                .HasMaxLength(20)
+                .HasColumnName("TerritoryID");
+
+            entity.Property(e => e.RegionId).HasColumnName("RegionID");
+
+            entity.Property(e => e.TerritoryDescription)
+                .HasMaxLength(50)
+                .IsFixedLength();
+
+            entity.HasOne(d => d.Region)
+                .WithMany(p => p.Territories)
+                .HasForeignKey(d => d.RegionId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Territories_Region");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ APIMiniProject/APIMiniProject/Models/Region.cs
@@ -0,0 +1,14 @@
+﻿namespace APIMiniProject.Models;
+
+public partial class Region
+{
+    public Region()
+    {
+        Territories = new HashSet<Territory>();
+    }
+
+    public int RegionId { get; set; }
+    public string RegionDescription { get; set; } = null!;
+
+    public virtual ICollection<Territory> Territories { get; set; }
+}
