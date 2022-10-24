5a89e568515291cca03b7f31e8156f80036c551c
GitAdamReed <57542237+MiniAddman@users.noreply.github.com>
2022-10-24 16:27:03 +0100

    Added tests project

---
+++ APIMiniProject/APIMiniProjectTests/ServiceTests.cs
@@ -0,0 +1,16 @@
+namespace APIMiniProjectTests
+{
+    public class ServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
