33cadac3adbaf8e439f27bb85a629df3928aaff5
Rob <robertma.green@gmail.com>
2022-10-25 10:18:10 +0100

    birthday

--- APIMiniProject/APIMiniProject/Models/DTOs/BirthdayDTO.cs
+++ APIMiniProject/APIMiniProject/Models/DTOs/BirthdayDTO.cs
@@ -1,14 +1,6 @@
 ﻿namespace APIMiniProject.Models.DTOs;
 
-public class BirthdayDTO
-{
-<<<<<<< HEAD
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public DateTime? BirthDate { get; set; }
-    public DateTime? UpcomingBirthday { get; set; }
-}
-=======
+
     public class BirthdayDTO
     {
         public string FirstName { get; set; } = null!;
@@ -17,5 +9,3 @@ public class BirthdayDTO
         public DateTime? UpcomingBirthday { get; set; }
         public int Age { get; set; }
     }
-}
->>>>>>> a58fd4a0f1233f43415658538c9228aa94d1564f
