4291cf422ea50210ed415664e96c9fdce38d13e2
Jai Kothari <jai25@live.co.uk>
2022-10-25 10:26:22 +0100

    Merge sorting.

--- APIMiniProject/APIMiniProject/Models/DTOs/BirthdayDTO.cs
+++ APIMiniProject/APIMiniProject/Models/DTOs/BirthdayDTO.cs
@@ -2,20 +2,9 @@
 
 public class BirthdayDTO
 {
-<<<<<<< HEAD
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
     public DateTime? BirthDate { get; set; }
     public DateTime? UpcomingBirthday { get; set; }
+    public int Age { get; set; }
 }
-=======
-    public class BirthdayDTO
-    {
-        public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
-        public DateTime? BirthDate { get; set; }
-        public DateTime? UpcomingBirthday { get; set; }
-        public int Age { get; set; }
-    }
-}
->>>>>>> a58fd4a0f1233f43415658538c9228aa94d1564f
